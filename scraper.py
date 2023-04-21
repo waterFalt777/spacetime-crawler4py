d696d516b719c16eb3baf21d1ba1552df3197ef6
steph <stephy888la7@gmail.com>
2023-04-21 01:36:47 -0700

    scraper fun

--- scraper.py
+++ scraper.py
@@ -1,6 +1,12 @@
 import re
 from urllib.parse import urlparse
 
+#class errors
+class statusError(exception):
+    '''Error for when status code is not 200 indication that there was an error with page retrieval.'''
+    "Status is not ok. Error in retrieving page."
+    pass
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -15,7 +21,23 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+   
+    hyperlinkLst = [] #list of hyperlinks declaration
+
+    #test for page
+    try:
+        if resp.status == 200:
+            #get response and go thru the content to get hyperlinks
+            pass
+        elif resp.status != 200:
+            print(resp.error)
+            raise statusError 
+
+    except statusError:
+        print("Status Error Raised")
+
+
+    return hyperlinkLst#list()
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
